e873185a3c4906f03add079a0753318160d5aea0
David Clarke <drclarke99@gmail.com>
2023-01-12 18:31:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const elements = document.querySelector('.menu__backdrop');
-const list = elements.classList;
-
-function openMenu() {
-    list.add("menu--open");
-    list.remove("menu--close");
-}
-
-function closeMenu() {
-    list.add("menu--close");
-    list.remove("menu--open");
-}
-
 /*function openMenu() {
     element.classList += " menu--open";
     element.classList.remove("menu--close");
