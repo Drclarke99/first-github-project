a978d3fb634b23c6ba9d00bf33c651945bc4912d
David Clarke <drclarke99@gmail.com>
2023-01-12 17:49:43 -0800

    added svg assets

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const element = document.querySelector('.menu__backdrop');
+const elements = document.querySelector('.menu__backdrop');
+const list = elements.classList;
 
 function openMenu() {
-    element.classList.add("menu--open");
+    list.add("menu--open");
+    list.remove("menu--close");
+}
+
+function closeMenu() {
+    list.add("menu--close");
+    list.remove("menu--open");
+}
+
+/*function openMenu() {
+    element.classList += " menu--open";
     element.classList.remove("menu--close");
 }
 
 function closeMenu() {
     element.classList.add("menu--close");
     element.classList.remove("menu--open");
-}
+}*/
